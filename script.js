1b2992f9a27ad3c19241881ba8108f30deb171a3
Justin Pope <justinmps12@gmail.com>
2024-06-27 22:35:08 -0400

    Update JavaScript

--- script.js
+++ script.js
@@ -20,3 +20,46 @@ function calculate() {
         display.value = "Error";
     }
 }
+
+function calculatePercent() {
+    try {
+        display.value = eval((display.value)/100);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
+
+function deleteValue() {
+    if (display.value != "") {
+        display.value = display.value.substring(0, display.value.length-1);
+    }
+}
+
+var colorIndex = 0;
+var backgroundColors = ["#f7cc38", "#ee638c", "#60a2d7", "#262626", "#ffffff"];
+var bgColor = document.querySelector('#calculator');
+function changeColors() {
+    if (colorIndex == backgroundColors.length) {
+        colorIndex = 0;
+    }
+    bgColor.style.backgroundColor = backgroundColors[colorIndex];
+    colorIndex++;
+}
+
+var displayColorIndex = 0;
+var displayBackgrounds = ["cyan", "magenta", "green", "purple", "#333333"];
+var displayColor = document.querySelector('#display');
+function changeDisplayColor() {
+    if (displayColorIndex == displayBackgrounds.length) {
+        displayColorIndex = 0;
+    }
+    displayColor.style.backgroundColor = displayBackgrounds[displayColorIndex];
+    displayColorIndex++;
+}
+
+function reset() {
+    bgColor.style.backgroundColor = "#262626";
+    displayColor.style.backgroundColor = "#333333";
+    clearDisplay();
+}
